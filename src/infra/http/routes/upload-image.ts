b248c97eef1a33b6efc428eba67d9ad39f043b81
Douglas <douglasmodolo@outlook.com.br>
2025-03-19 22:14:16 -0300

    Upload de imagem

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -9,10 +9,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
     {
       schema: {
         summary: 'Upload an image',
-        body: z.object({
-          name: z.string(),
-          password: z.string().optional(),
-        }),
+        consumes: ['multipart/form-data'],
         response: {
           201: z.object({ uploadId: z.string() }),
           409: z
@@ -22,11 +19,13 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       },
     },
     async (request, reply) => {
-      await db.insert(schema.uploads).values({
-        name: request.body.name,
-        remoteKey: 'test',
-        remoteUrl: 'http://localhost:3000/test',
+      const uploadedFile = await request.file({
+        limits: {
+          fileSize: 1024 * 1024 * 2, // 2MB
+        },
       })
+
+      console.log(uploadedFile)
 
       return reply.status(201).send({ uploadId: 'test' })
     }
